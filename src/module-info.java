edb21b3db14fe9f16f656d09d4386ade0dfbd7d4
marbit <bitkowskamaria@gmail.com>
2025-03-28 18:03:22 +0100

    pierwszy i pół commit

--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,3 @@
 module JavaProjekt {
+	requires java.desktop;
 }
---
+++ src/pl/edu/pw/fizyka/pojava/BitkowskaKysiak/WelcomePanel.java
@@ -0,0 +1,26 @@
+package pl.edu.pw.fizyka.pojava.BitkowskaKysiak;
+
+import javax.swing.JPanel;
+
+public class WelcomePanel extends JPanel{
+	
+	private static final long serialVersionUID = 1L;
+
+	
+	public WelcomePanel() {
+		super();
+		
+		
+		
+		
+		
+	}
+
+
+	
+
+	public static void main(String[] args) {
+
+	}
+
+}
