f1fc8291619f0e9a86bb576d4faea6a9294a0537
marbitek <bitkowskamaria@gmail.com>
2025-03-28 22:00:51 +0100

    4 commit

--- src/pl/edu/pw/fizyka/pojava/BitkowskaKysiak/MainPanel.java
+++ src/pl/edu/pw/fizyka/pojava/BitkowskaKysiak/MainPanel.java
@@ -1,15 +1,12 @@
 package pl.edu.pw.fizyka.pojava.BitkowskaKysiak;
 
-import java.awt.BorderLayout;
+
 import java.awt.CardLayout;
 import java.awt.Color;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
+
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 
 public class MainPanel extends JFrame {
@@ -18,61 +15,33 @@ public class MainPanel extends JFrame {
 
 	static JPanel homePanel, centre;
 	static JPanel homeContainer;
-	static JPanel welcomePanel;
+	static WelcomePanel welcomePanel;
+	static GamePanel gamePanel;
 	static CardLayout card;
-	JButton startButton;
-	JButton backButton;
-	JLabel welcome, nick;
-	JTextArea username;
-	
 	
 	
 	public MainPanel()  {
 		JFrame frame = new JFrame("Dune Harmonics");
 		
-		
-		startButton = new JButton("Start");
-		backButton = new JButton("Back");
-		
 		card = new CardLayout(5, 5);
 		homeContainer = new JPanel(card);
 		homeContainer.setBackground(Color.white);
 		
-	    homePanel = new JPanel(new BorderLayout());
-	    homePanel.setBackground(Color.white);
-	    homePanel.add(startButton, BorderLayout.SOUTH);
-	    welcome = new JLabel("Welcome to Dune Harmonics!");
-	    homePanel.add(welcome, BorderLayout.PAGE_START);
-	    nick = new JLabel("Please insert you nick");
-	    username = new JTextArea();
-	    centre = new JPanel();
-	    centre.setLayout(new BoxLayout(centre, BoxLayout.Y_AXIS));
-	    homePanel.add(centre, BorderLayout.CENTER);
-	    centre.add(nick);
-	    centre.add(username);
-	    
-	    
-	    
-	    
-	    
-
-	    homeContainer.add(homePanel, "Home");
-
-	    welcomePanel = new JPanel();
-	    welcomePanel.setBackground(Color.green);
-	    welcomePanel.add(backButton);
-
+	    welcomePanel = new WelcomePanel();
 	    homeContainer.add(welcomePanel, "Welcome Panel");
+	    welcomePanel.start.addActionListener(e -> card.show(homeContainer, "Game Panel"));
+	    
+	    gamePanel = new GamePanel();
+	    homeContainer.add(gamePanel, "Game Panel");
+	    gamePanel.back.addActionListener(e -> card.show(homeContainer, "Welcome Panel"));
 
-	    startButton.addActionListener(e -> card.show(homeContainer, "Welcome Panel"));
-	    backButton.addActionListener(e -> card.show(homeContainer, "Home"));
-
+	    
 	    frame.add(homeContainer);
-	    card.show(homeContainer, "Home");
+	    card.show(homeContainer, "Welcome Panel");
 	    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-	    frame.setSize(640, 480);
+	    frame.setSize(840, 580);
 	    frame.setLocationRelativeTo(null);
-	    frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+	    //frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 	    //frame.pack();
 	    frame.setVisible(true);
 	}
--- src/pl/edu/pw/fizyka/pojava/BitkowskaKysiak/WelcomePanel.java
+++ src/pl/edu/pw/fizyka/pojava/BitkowskaKysiak/WelcomePanel.java
@@ -1,15 +1,68 @@
 package pl.edu.pw.fizyka.pojava.BitkowskaKysiak;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class WelcomePanel extends JPanel{
 	
 	private static final long serialVersionUID = 1L;
 
 	
+	protected JButton start;
+	private JPanel center;
+	private JLabel welcome, nickname;
+	private JTextField insertNick;
+	static int fontSize = 32;
+	
+	
 	public WelcomePanel() {
-		super();
+		super(new BorderLayout());
+		this.setBackground(Color.orange);
 		
+		start = new JButton("Start");
+		
+		welcome = new JLabel("Welcome in Dune Harmonics!");
+		welcome.setFont(new Font("Helvetica", Font.BOLD, fontSize));
+		welcome.setVerticalAlignment(SwingConstants.CENTER);
+		welcome.setHorizontalAlignment(SwingConstants.CENTER);
+		
+	
+		
+		center = new JPanel();
+		center.setLayout(new FlowLayout(FlowLayout.CENTER));
+		center.setOpaque(false); // przezroczysty, żeby kolor z tła był widoczny
+
+		insertNick = new JTextField(20);
+		insertNick.setPreferredSize(new Dimension(200, 30)); // ✅ ograniczenie rozmiaru
+
+		nickname = new JLabel("Please enter your nickname:");
+		center.add(nickname);
+		center.add(insertNick);
+
+		
+		center.add(nickname);
+		center.add(insertNick);
+		nickname.setAlignmentX(Component.CENTER_ALIGNMENT);
+		insertNick.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		
+		this.add(welcome, BorderLayout.NORTH);
+		this.add(center, BorderLayout.CENTER);
+		this.add(start, BorderLayout.SOUTH);
+		
+		
+		 
 		
 		
 		
@@ -18,6 +71,10 @@ public class WelcomePanel extends JPanel{
 
 
 	
+
+	
+
+
 
 	public static void main(String[] args) {
 
