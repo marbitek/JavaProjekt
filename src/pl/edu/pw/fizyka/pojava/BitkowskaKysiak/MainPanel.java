a9a42e36b9279d51a110b0dfc79d5c47a763b6bf
mkysiak <01187731@pw.edu.pl>
2025-03-31 16:49:43 +0200

    welcoming the world

--- src/pl/edu/pw/fizyka/pojava/BitkowskaKysiak/MainPanel.java
+++ src/pl/edu/pw/fizyka/pojava/BitkowskaKysiak/MainPanel.java
@@ -48,7 +48,7 @@ public class MainPanel extends JFrame {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hello!");
+		System.out.println("Hello World!");
 	    SwingUtilities.invokeLater(MainPanel::new);
 	}
 	
