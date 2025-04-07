2f62e471934c03b095eebeab681e383eeef2e7bf
mkysiak <01187731@pw.edu.pl>
2025-04-07 20:01:13 +0200

    messing with the sim panel

--- src/pl/edu/pw/fizyka/pojava/BitkowskaKysiak/SimulationPanel.java
+++ src/pl/edu/pw/fizyka/pojava/BitkowskaKysiak/SimulationPanel.java
@@ -10,7 +10,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-
+import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
 
 //JPanel w GamePanel'u w ktorym odbywac sie bedzie symulacja
 public class SimulationPanel extends JPanel
@@ -18,12 +20,29 @@ public class SimulationPanel extends JPanel
 	int x_dim = 100;
 	int y_dim = 100;
 	Pixel[][] pixels; 
+	List<List<Pixel>> pixelGrid; //tablice wierszy siatki pixeli
+	List<Pixel> pixelrow;
 	int pixelSize = 2;
 	
 	public SimulationPanel()
 	{
 		this.setMinimumSize(new Dimension(x_dim*pixelSize, y_dim*pixelSize));
+		List<List<Pixel>> pixelGrid = new ArrayList<>();
+		
+		for(int y = 0; y < y_dim/pixelSize; y++)
+		{
+			pixelrow = new ArrayList<>();
+				for(int x = 0; x < x_dim/pixelSize; x++)
+				{
+					int X = x * pixelSize;
+		            int Y = y * pixelSize;
+		            pixelrow.add(new Pixel(X, Y));
+				}
+		}
+		
+		/*
 		pixels = new Pixel[x_dim][y_dim];
+		
 		
 		for(int x = 0; x < x_dim; x++) 
 		{
@@ -32,6 +51,7 @@ public class SimulationPanel extends JPanel
 				pixels[x][y] = new Pixel(x, y);
 			};
 		};
+		*/
 	}
 	
     @Override
