966ac7ea32b296bc280f7e279c6691ec750156d2
mkysiak <01187731@pw.edu.pl>
2025-04-16 09:30:55 +0200

    changing the simulation panel to an image - work in progress

--- src/pl/edu/pw/fizyka/pojava/BitkowskaKysiak/Pixel.java
+++ src/pl/edu/pw/fizyka/pojava/BitkowskaKysiak/Pixel.java
@@ -11,33 +11,78 @@ import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
-public class Pixel extends JPanel
+public class Pixel //extends JPanel
 {
 	Color color = Color.white;
-	int x, y;
+	int x, y, vx, vy;
+	String[] terrainTypes = { "Sand", "Granite", "Limestone"};
+	String thisTerrainType = terrainTypes[0];
 	
-    public Pixel(int x, int y, Color c) 
+    public Pixel(int x, int y, Color c, int terrainSpecificator) 
     {
         this.x = x;
         this.y = y;
         this.color = c;
-        this.setBackground(color);
-        this.setMinimumSize(new Dimension(x,y));
-        this.setPreferredSize(new Dimension(x,y));
+        //this.setBackground(color);
+        //this.setMinimumSize(new Dimension(x,y));
+        //this.setPreferredSize(new Dimension(x,y));
+        
+        if(terrainSpecificator >= 0 || terrainSpecificator <= 2)
+        {
+        	thisTerrainType = terrainTypes[terrainSpecificator];
+        }
+        
     }
     
     //public Color getColor() {return color;}
     
+    
+    /*
     public void setColor(Color color) 
     {
         this.color = color;
         setBackground(color);
         repaint();
-    }
+    }*/
     
     public void draw(Graphics g, int size) 
     {
         g.setColor(color);
         g.fillRect(x * size, y * size, size, size);
     }
+    
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getVx() {
+        return vx;
+    }
+
+    public void setVx(int vx) {
+        this.vx = vx;
+    }
+
+    public int getVy() {
+        return vy;
+    }
+
+    public void setVy(int vy) {
+        this.vy = vy;
+    }
+
+    public Color getClr() {return color;}
+    public void setClr(Color c) {color = c;}
 }
--- src/pl/edu/pw/fizyka/pojava/BitkowskaKysiak/SimulationPanel.java
+++ src/pl/edu/pw/fizyka/pojava/BitkowskaKysiak/SimulationPanel.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.image.BufferedImage;
 import java.util.*;
 import java.util.List;
 
@@ -21,36 +22,67 @@ public class SimulationPanel extends JPanel
 	//Pixel[][] pixels; 
 	List<List<Pixel>> pixelGrid; //tablice wierszy siatki pixeli
 	List<Pixel> pixelrow;
-	int pixelSize = 2;
+	int pixelSize = 2, imgW, imgH;
 	Pixel onePxl;
 	Random rand = new Random();
 	float R, G, B;
+	BufferedImage panelImage = null;
 	
 	public SimulationPanel(int X, int Y, int size)
 	{
 		x_dim = X;
 		y_dim = Y;
 		pixelSize = size;
-		this.setLayout(new GridLayout(x_dim,y_dim));
+		
+		//this.setLayout(new GridLayout(x_dim,y_dim));
 		pixelGrid = new ArrayList<>();
+		imgW = x_dim*pixelSize;
+		imgH = y_dim*pixelSize;
+		
+		//naszym panelem bedzie obrazek, powinno to rozwiązać problemy z pamięcią
+		//przy torzeniu 10000 paneli XD
+		panelImage = new BufferedImage(imgW, imgH, BufferedImage.TYPE_INT_RGB);
+		
+        Graphics2D g2d = panelImage.createGraphics();
+        g2d.setColor(new Color(255, 228, 181));
+        g2d.fillRect(0, 0, imgW, imgH);
+        g2d.dispose();
+
+        setPreferredSize(new Dimension(imgW, imgH));
 		
 		for(int y = 0; y<y_dim; y++)
 		{
 			pixelrow = new ArrayList<>();
 			for(int x = 0; x < x_dim; x++)
 			{
-				R=rand.nextFloat();
-				G=rand.nextFloat();
-				B=rand.nextFloat();
-				onePxl = new Pixel(pixelSize, pixelSize, new Color(R,G,B));
-				this.add(onePxl);
+				onePxl = new Pixel(pixelSize, pixelSize, new Color(
+						rand.nextInt(),rand.nextInt(),rand.nextInt()
+						), 0);
+				
+				//this.add(onePxl);
 				pixelrow.add(onePxl);
+				this.paintPxl(onePxl.getX(), onePxl.getY(), onePxl.getClr());
 			}
 			pixelGrid.add(pixelrow);
 		}
+		this.repaint();
 	}
 	
-	public Pixel getPixel(int x, int y)
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(panelImage, 0, 0, null);
+    }
+    
+    public void paintPxl(int Width, int Height, Color c)
+    {
+        Graphics2D g2d = panelImage.createGraphics();
+        g2d.setColor(c);
+        g2d.fillRect(Width*pixelSize, Height*pixelSize, pixelSize, pixelSize);
+        g2d.dispose();
+    }
+	
+	public Pixel getPxl(int x, int y) //get pixel
 	{
 		Pixel p = pixelGrid.get(y).get(x);
 		return p;
