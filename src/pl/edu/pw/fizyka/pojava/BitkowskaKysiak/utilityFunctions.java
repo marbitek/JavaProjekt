56736852ac533082033991dd389cc284f9cbc441
mkysiak <01187731@pw.edu.pl>
2025-04-17 19:05:13 +0200

    added missing classes and interface

--- src/pl/edu/pw/fizyka/pojava/BitkowskaKysiak/utilityFunctions.java
+++ src/pl/edu/pw/fizyka/pojava/BitkowskaKysiak/utilityFunctions.java
@@ -2,34 +2,15 @@ package pl.edu.pw.fizyka.pojava.BitkowskaKysiak;
 
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
-
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.DefaultListCellRenderer;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JSlider;
-import javax.swing.SwingConstants;
 import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class utilityFunctions extends JFrame {
 	
