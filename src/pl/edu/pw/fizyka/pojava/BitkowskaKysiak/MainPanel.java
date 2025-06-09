45291ed8a8ec19c2323408e903b8bc34aca0f9a8
marbitek <bitkowskamaria@gmail.com>
2025-06-09 19:49:36 +0200

    połączenie z bazą w oddzielnym wątku

--- src/pl/edu/pw/fizyka/pojava/BitkowskaKysiak/MainPanel.java
+++ src/pl/edu/pw/fizyka/pojava/BitkowskaKysiak/MainPanel.java
@@ -9,6 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
 
 public class MainPanel extends JFrame {
 
@@ -23,7 +24,12 @@ public class MainPanel extends JFrame {
 	
 	public MainPanel() throws SQLException  
 	{
-		JFrame frame = new JFrame("Dune Harmonics");	
+		JFrame frame = new JFrame("Dune Harmonics");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(900, 600);
+		frame.setLocationRelativeTo(null);
+		
+		
 		card = new CardLayout(5, 5);
 		homeContainer = new JPanel(card);
 		homeContainer.setBackground(Color.white);
@@ -32,41 +38,69 @@ public class MainPanel extends JFrame {
 	    homeContainer.add(welcomePanel, "Welcome Panel");
 	    //welcomePanel.start.addActionListener(e -> card.show(homeContainer, "Game Panel"));
 	    
-	    baza = new BazaDanych();
+	    //baza = new BazaDanych();
+	    
+        new SwingWorker<BazaDanych, Void>() {
+            @Override
+            protected BazaDanych doInBackground() throws Exception {
+                // tu dopiero tworzymy połączenie (ciężka operacja)
+                return new BazaDanych();
+            }
+            @Override
+            protected void done() {
+                try {
+                    baza = get();                   // wynik new BazaDanych()
+                    welcomePanel.start.setEnabled(true);
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(
+                        MainPanel.this,
+                        "Nie udało się połączyć z bazą:\n" + ex.getMessage(),
+                        "Błąd połączenia",
+                        JOptionPane.ERROR_MESSAGE
+                    );
+                    System.exit(1);
+                }
+            }
+        }.execute();
 	    
 	    welcomePanel.start.addActionListener(e -> {
 	        String nick = welcomePanel.insertNick.getText().trim();
-	        if (nick.isEmpty()) 
-	        {
-	            nick = "Stranger";
-	        }
-
-	        String info = String.format("""
-	            Welcome %s in Dune Harmonics!
-	            
-	            In this game you can:
-	            • Chose the type of field,
-	            • Chose the number of sources,
-	            • Set the frequency of the sources,
-	            • Observe the data.
-
-	            Click OK to start the game!
-	                 """, nick);
-
-
-	        javax.swing.JOptionPane.showMessageDialog(
-	            homeContainer,
-	            info,
-	            "Instruction",
-	            javax.swing.JOptionPane.INFORMATION_MESSAGE
-	        );
 	        
-	        try {
-				baza.addUser(nick);
-			} catch (SQLException e1) {
-				e1.printStackTrace();
-			}
-	        card.show(homeContainer, "Game Panel");
+	        String nickFinal = nick.isEmpty() ? "Stranger" : nick;
+	      
+	        welcomePanel.start.setEnabled(false);
+
+	        new SwingWorker<Void, Void>() {
+                @Override
+                protected Void doInBackground() throws Exception {
+                    baza.addUser(nickFinal);
+                    return null;
+                }
+                @Override
+                protected void done() {
+                    welcomePanel.start.setEnabled(true);
+                    // komunikat i przejście na panel gry
+                    String info = String.format("""
+                        Welcome %s in Dune Harmonics!
+
+                        In this game you can:
+                        • Chose the type of field,
+                        • Chose the number of sources,
+                        • Set the frequency of the sources,
+                        • Observe the data.
+
+                        Click OK to start the game!
+                             """, nickFinal);
+                    JOptionPane.showMessageDialog(
+                        MainPanel.this,
+                        info,
+                        "Instruction",
+                        JOptionPane.INFORMATION_MESSAGE
+                    );
+                    card.show(homeContainer, "Game Panel");
+                }
+            }.execute();
+            
 	    });
 
 	    
@@ -87,9 +121,7 @@ public class MainPanel extends JFrame {
 	    
 	    frame.add(homeContainer);
 	    card.show(homeContainer, "Welcome Panel");
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.setSize(900, 600);
-	    frame.setLocationRelativeTo(null);
+	   
 	    //frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 	    //frame.pack();
 	    frame.setVisible(true);
